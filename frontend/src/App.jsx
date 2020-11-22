edc75d9932eb63d870c1fd22ddb319a9571c9254
Avishek Mondal <avishek.mondal53@gmail.com>
2020-11-22 19:57:18 +0530

    added pose

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,133 +13,124 @@ import HearingIcon from "@material-ui/icons/Hearing";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import SpeakerNotesIcon from "@material-ui/icons/SpeakerNotes";
 import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import AccessibilityNewIcon from "@material-ui/icons/AccessibilityNew";
 
 import Layout from "./layout";
 import Home from "./pages/Home";
 import Signin from "./pages/SignIn";
 import Dashboard from "./components/Dashboard";
 import Hospitals from "./components/Hospitals";
+import Exercise from "./components/Exercise";
 
 import UserContext from "./context/UserContext";
 import { ProtectedRoute } from "./protected";
 
 function About() {
-	return (
-		<>
-			<h1>about</h1>
-		</>
-	);
+  return (
+    <>
+      <h1>about</h1>
+    </>
+  );
 }
 
 const componentList = [
-	{
-		name: "Dashboard",
-		url: "/",
-		component: Dashboard,
-		icon: DashboardIcon,
-	},
-	{
-		name: "Meal Plan",
-		url: "/meal-plan",
-		component: About,
-		icon: FastfoodIcon,
-	},
-	{
-		name: "Mobility assistance",
-		url: "/mobility-assistance",
-		component: About,
-		icon: AccessibleIcon,
-	},
-	{
-		name: "Hearing Test",
-		url: "/hearing-test",
-		component: HearingTest,
-		icon: HearingIcon,
-	},
-	{
-		name: "Eye Test",
-		url: "/eye-test",
-		component: About,
-		icon: VisibilityIcon,
-	},
-	{
-		name: "Delivery Cooking",
-		url: "/delivery-cooking",
-		component: About,
-		icon: ShoppingCartIcon,
-	},
-	{
-		name: "Forum",
-		url: "/forum",
-		component: About,
-		icon: SpeakerNotesIcon,
-	},
-	{
-		name: "Hospitals",
-		url: "/hospitals",
-		component: Hospitals,
-		icon: LocalHospitalIcon,
-	},
+  {
+    name: "Dashboard",
+    url: "/",
+    component: About,
+    // Dashboard,
+    icon: DashboardIcon,
+  },
+  {
+    name: "Meal Plan",
+    url: "/meal-plan",
+    component: About,
+    icon: FastfoodIcon,
+  },
+  {
+    name: "Hearing Test",
+    url: "/hearing-test",
+    component: HearingTest,
+    icon: HearingIcon,
+  },
+  {
+    name: "Delivery Cooking",
+    url: "/delivery-cooking",
+    component: About,
+    icon: ShoppingCartIcon,
+  },
+  {
+    name: "Exercise",
+    url: "/exercise",
+    component: Exercise,
+    icon: AccessibilityNewIcon,
+  },
+  {
+    name: "Hospitals",
+    url: "/hospitals",
+    component: Hospitals,
+    icon: LocalHospitalIcon,
+  },
 ];
 
 const App = () => {
-	const [userData, setUserData] = useState({
-		token: undefined,
-		user: undefined,
-	});
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
 
-	const checkLoggedIn = async () => {
-		let token = localStorage.getItem("auth-token");
-		if (token === null) {
-			localStorage.setItem("auth-token", "");
-			token = "";
-		}
-		const tokenRes = await Axios.post("is-token-valid", null, {
-			headers: { "x-auth-token": token },
-		});
+  const checkLoggedIn = async () => {
+    let token = localStorage.getItem("auth-token");
+    if (token === null) {
+      localStorage.setItem("auth-token", "");
+      token = "";
+    }
+    const tokenRes = await Axios.post("is-token-valid", null, {
+      headers: { "x-auth-token": token },
+    });
 
-		if (tokenRes.data) {
-			const userRes = await Axios.get("get-current-patient", {
-				headers: { "x-auth-token": token },
-			});
+    if (tokenRes.data) {
+      const userRes = await Axios.get("get-current-patient", {
+        headers: { "x-auth-token": token },
+      });
 
-			setUserData({
-				token: token,
-				user: userRes.data,
-			});
-		}
-	};
+      setUserData({
+        token: token,
+        user: userRes.data,
+      });
+    }
+  };
 
-	useEffect(() => {
-		checkLoggedIn();
-	}, []);
+  useEffect(() => {
+    checkLoggedIn();
+  }, []);
 
-	return (
-		<IonApp>
-			<CssBaseline />
-			<UserContext.Provider value={{ userData, setUserData }}>
-				<BrowserRouter>
-					<Switch>
-						<Route
-							path="/signin"
-							exact
-							render={(props) => <Signin {...props} />}
-						/>
-						<Layout componentList={componentList}>
-							{componentList.map((item, key) => (
-								<ProtectedRoute
-									path={item.url}
-									key={key}
-									exact={true}
-									component={item.component}
-								/>
-							))}
-						</Layout>
-					</Switch>
-				</BrowserRouter>
-			</UserContext.Provider>
-		</IonApp>
-	);
+  return (
+    <IonApp>
+      <CssBaseline />
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <BrowserRouter>
+          <Switch>
+            <Route
+              path="/signin"
+              exact
+              render={(props) => <Signin {...props} />}
+            />
+            <Layout componentList={componentList}>
+              {componentList.map((item, key) => (
+                <ProtectedRoute
+                  path={item.url}
+                  key={key}
+                  exact={true}
+                  component={item.component}
+                />
+              ))}
+            </Layout>
+          </Switch>
+        </BrowserRouter>
+      </UserContext.Provider>
+    </IonApp>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Exercise/index.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import CloseIcon from "@material-ui/icons/Close";
+import Slide from "@material-ui/core/Slide";
+
+import Squats from "./Squats";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: "relative",
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+}));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function Exercise() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Open Squats
+      </Button>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar className={classes.appBar} color="secondary">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Sound
+            </Typography>
+            <Button autoFocus color="inherit" onClick={handleClose}>
+              save
+            </Button>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Squats />
+      </Dialog>
+    </div>
+  );
+}
