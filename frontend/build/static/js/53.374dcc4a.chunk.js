92e7aa102a12eecc2d4342685bf287882898fabb
Avishek Mondal <avishek.mondal53@gmail.com>
2020-11-23 14:43:00 +0530

    deleted build

--- frontend/build/static/js/53.374dcc4a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[53],{1069:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(88),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var r=i.closest("ion-content");r&&r.componentOnReady().then((function(){Object(o.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=53.374dcc4a.chunk.js.map
